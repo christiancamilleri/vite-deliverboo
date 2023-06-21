916b557a27266cd6a0662187a0ca7279a95abf55
christianarca27 <christian.arca.mail@gmail.com>
2023-06-21 10:12:48 +0200

    feat: inserita funzione di pagamento lato front end tramite drop in Braintree

--- src/components/AppCart.vue
+++ src/components/AppCart.vue
@@ -1,34 +1,31 @@
 <script>
 import { store } from '../store.js';
 
+import AppPayment from './AppPayment.vue';
 export default {
-    name: 'Cart',
-
+    name: "Cart",
     data() {
         return {
             store,
         };
     },
-
     methods: {
         addToCart(item) {
             if (this.store.cartItems.length) {
-                if (this.store.restaurant_id == item['restaurant_id']) {
+                if (this.store.restaurant_id == item["restaurant_id"]) {
                     this.store.cartItems.push(item);
                     this.saveCartItems();
                 }
                 else {
-                    alert('Stai ordinando in un altro ristorante');
+                    alert("Stai ordinando in un altro ristorante");
                 }
             }
             else {
-                this.store.restaurant_id = item['restaurant_id'];
+                this.store.restaurant_id = item["restaurant_id"];
                 this.store.cartItems.push(item);
-
                 this.saveCartItems();
             }
         },
-
         removeFromCart(item) {
             const index = this.store.cartItems.indexOf(item);
             if (index !== -1) {
@@ -36,35 +33,33 @@ export default {
                 this.saveCartItems();
             }
         },
-
         saveCartItems() {
-            localStorage.setItem('cartItems', JSON.stringify(this.store.cartItems));
-            localStorage.setItem('restaurant_id', this.store.restaurant_id);
+            localStorage.setItem("cartItems", JSON.stringify(this.store.cartItems));
+            localStorage.setItem("restaurant_id", this.store.restaurant_id);
             this.updateTotalPrice();
         },
-
         updateTotalPrice() {
             let totalPrice = 0;
-
             this.store.cartItems.forEach(item => {
                 totalPrice += parseFloat(item.price);
             });
-
             this.store.totalPrice = totalPrice;
-            localStorage.setItem('total_price', this.store.totalPrice);
+            localStorage.setItem("total_price", this.store.totalPrice);
         },
-
         emptyCart() {
             this.store.cartItems = [];
             this.saveCartItems();
-        }
+        },
     },
+    components: {
+        AppPayment
+    }
 };
 </script>
 
 <template>
     <div class="container">
-        <div v-if="store.cartItems.length">
+        <div class="mb-3" v-if="store.cartItems.length">
             <ul class="list-group">
                 <li class="list-group-item d-flex justify-content-between align-items-center"
                     v-for="item in store.cartItems">
@@ -80,9 +75,11 @@ export default {
             <button @click="emptyCart()" class="btn btn-primary">Svuota carrello</button>
         </div>
 
-        <div v-else class="alert alert-info" role="alert">
+        <div v-else class="alert alert-info mb-3" role="alert">
             Carrello vuoto.
         </div>
+
+        <AppPayment></AppPayment>
     </div>
 </template>
 
---
+++ src/components/AppPayment.vue
@@ -0,0 +1,87 @@
+<script>
+import dropin from 'braintree-web-drop-in';
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            dropinInstance: null,
+
+            paymentSuccess: false,
+
+            paymentMessage: '',
+
+            paymentLoading: false,
+        }
+    },
+
+    methods: {
+        payment() {
+            if (this.dropinInstance.isPaymentMethodRequestable()) {
+                this.paymentLoading = true;
+
+                this.dropinInstance.requestPaymentMethod((err, payload) => {
+                    if (err) {
+                        // Handle errors in requesting payment method
+                        console.log('payment fallito');
+                        return;
+                    }
+
+                    // Send payload.nonce to your server
+                    axios.post('http://127.0.0.1:8000/api/braintree/payment', { payment_method_nonce: payload.nonce, amount: store.totalPrice }).then(res => {
+                        this.paymentSuccess = res.data.success;
+                        this.paymentMessage = res.data.message;
+                        this.paymentLoading = false;
+
+                        this.emptyCart();
+                    });
+                });
+            }
+        },
+    },
+
+    mounted() {
+        axios.post('http://127.0.0.1:8000/api/braintree/token').then(res => {
+            let clientToken = res.data.clientToken;
+
+            // Step two: create a dropin instance using that container (or a string
+            //   that functions as a query selector such as `#dropin-container`)
+            dropin.create({
+                authorization: clientToken,
+                container: '#dropin-container',
+                locale: 'it_IT',
+                // ...plus remaining configuration
+            }, (error, dropinInstance) => {
+                // Use `dropinInstance` here
+                // Methods documented at https://braintree.github.io/braintree-web-drop-in/docs/current/Dropin.html
+                this.dropinInstance = dropinInstance;
+            });
+        });
+    },
+}
+</script>
+
+<template>
+    <div id="payment">
+        <div v-show="!paymentMessage" id="dropin-container"></div>
+
+        <div v-show="paymentLoading">
+            <div class="spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+
+        <div v-show="!paymentLoading">
+
+            <div v-show="paymentMessage" class="alert" :class="paymentSuccess ? 'alert-success' : 'alert-danger'"
+                role="alert">
+                {{ paymentMessage }}
+            </div>
+
+            <button v-show="!paymentMessage" @click="payment()" id="submit-button" class="btn btn-primary mb-3"
+                :class="dropinInstance == null ? 'disabled' : ''">Ordina</button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
