15b6a0a006e3b10806647c6d3a614e7d285a33fe
giovannitamburini <giovatambu@gmail.com>
2023-06-26 18:16:43 +0200

    added shadow to restaurant cards

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -4,7 +4,6 @@ export default {
 
     data() {
         return {
-
         }
     },
 
@@ -20,15 +19,15 @@ export default {
                 return 'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg'
             }
         }
-    }
+    },
 }
 </script>
 
 <template>
-    <div class="card h-100">
-        <img :src="img" class="card-img-top" alt="">
+    <div v-on:mouseover="overRestaurant()" v-on:mouseleave="leaveRestaurant()" class="card h-100 my-restaurant-card">
+        <img :src="img" class="card-img-top" alt="restaurant-img">
         <div class="card-body d-flex flex-column justify-content-between">
-            <h5 class="card-title">{{ restaurant.name }}</h5>
+            <h5 class="card-title my-restaurant-title">{{ restaurant.name }}</h5>
             <p class="card-text">
                 <span v-for="typology in restaurant.typologies" class="badge"
                     :style="{ backgroundColor: typology.color }">{{ typology.name }}</span>
@@ -41,8 +40,24 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-img {
-    height: 300px;
-    object-fit: cover;
+.my-restaurant-card {
+    box-shadow: 6px 6px 5px rgba($color: #000000, $alpha: 0.5);
+
+    transition: transform .5s;
+
+    &:hover {
+        transform: scale(1.2);
+        z-index: 1;
+    }
+
+    img {
+        height: 300px;
+        object-fit: cover;
+    }
+
+    .my-restaurant-title {
+        font-weight: 600;
+        text-shadow: 1px 1px rgba($color: #000000, $alpha: 0.5);
+    }
 }
 </style>
